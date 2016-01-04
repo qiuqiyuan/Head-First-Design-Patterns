55a250323fb59bd401753e0a70fb8b279c14619b
Qiyuan Qiu <qiuqiyuan@gmail.com>
2016-01-04 13:55:22 -0500

    Update class design
    According to description from sample program in head first design patterns.
    
    Major changes:
    Add in descrition field.
    
    Major differenct to code in the book:
    1. Beverage conponent placed in Decorator abstract class not in concrete class (like Whip).
        beverage coponent is not in concrete class because I believe in the abstract class is
        more obvious that all decorator sub-class is decorator to Beverage.

--- practice/decorator/StarBuzz.java
+++ practice/decorator/StarBuzz.java
@@ -1,12 +1,16 @@
 abstract class Beverage {
-    private String description = "Unknown beverage";
-    public double cost() {
-        return 0.0;
-    };
+    protected String description = "Unknown beverage";
+    public String getDescription() {
+        return description;
+    }
+    public abstract double cost();
 }
 
 
 class HouseBlend extends Beverage {
+    HouseBlend () {
+        this.description = "HouseBlend";
+    }
     @Override
         public double cost() {
             return 0.89;
@@ -14,6 +18,9 @@ class HouseBlend extends Beverage {
 }
 
 class DarkRoast extends Beverage {
+    DarkRoast () {
+        this.description = "DarkRoast";
+    }
     @Override
         public double cost() {
             return 0.99;
@@ -21,6 +28,9 @@ class DarkRoast extends Beverage {
 }
 
 class Decaf extends Beverage {
+    Decaf () {
+        this.description = "Decaf";
+    }
     @Override
         public double cost() {
             return 1.05;
@@ -28,20 +38,28 @@ class Decaf extends Beverage {
 }
 
 class Espresso extends Beverage {
+    Espresso () {
+        this.description = "Espresso";
+    }
     @Override
         public double cost() {
             return 1.99;
         }
 }
 
-
 abstract class CondimentsDecorator extends Beverage {
-    protected Beverage beverage;
+    protected Beverage beverage; // For decotrator pattern
+    public abstract String getDescription();
 }
 
 class Whip extends CondimentsDecorator {
+
     public Whip (Beverage beverage){
         this.beverage = beverage;
+    }
+
+    public String getDescription () {
+        return this.beverage.getDescription() + " Whip";
     }
 
     public double cost() {
@@ -53,6 +71,11 @@ class Mocha extends CondimentsDecorator {
     public Mocha (Beverage beverage) {
         this.beverage = beverage;
     }
+
+    public String getDescription () {
+        return this.beverage.getDescription() + " Mocha";
+    }
+
     public double cost() {
         return 0.20 + beverage.cost();
     }
@@ -62,6 +85,11 @@ class StreamedMilk extends CondimentsDecorator {
     public StreamedMilk (Beverage beverage) {
         this.beverage = beverage; 
     }
+
+    public String getDescription () {
+        return this.beverage.getDescription() + " SteamMilk";
+    }
+
     public double cost() {
         return 0.10 + beverage.cost();
     }
@@ -69,8 +97,7 @@ class StreamedMilk extends CondimentsDecorator {
 
 public class StarBuzz {
     public static void main(String[] args){
-        Beverage doubleMochaDarkRoast = new Mocha(new Mocha(new DarkRoast()));
-        System.out.println("Double Mocha DarkRoast costs: ");
-        System.out.println(doubleMochaDarkRoast.cost());
+        Beverage drink1 = new Mocha(new Mocha(new DarkRoast()));
+        System.out.println( drink1.getDescription() + " costs: " + drink1.cost());
     }
 }
